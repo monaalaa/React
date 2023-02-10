924cfe250ca40ac2e3f66be19b9d3e30297b694e
monaalaa <106662972+MonaBindary1@users.noreply.github.com>
2023-02-10 08:24:59 +0100

    creating input search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor()
+  {
+    super();
+    this.state={
+      monsters:[],
+      searchValue:''
+    };
+  }
+
+  componentDidMount(){
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then((response)=>response.json())
+     .then((users)=>this.setState(()=>{
+      return{ monsters: users}
+     },()=>{
+      console.log(this.state);
+     })
+     ) 
+  }
+  OnsearchChanged=(event)=>{
+    const searchValue=event.target.value.toLocaleLowerCase();
+    this.setState(()=>{ return{searchValue}}
+    )
+  };
+  render()
+  {
+ const {monsters,searchValue}=this.state;
+ const {OnsearchChanged}=this;
+    const data=monsters.filter
+    (monster=>monster.name.toLocaleLowerCase().includes(searchValue));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className='searchBox' type='search' placeholder='search mosnters' 
+      onChange={OnsearchChanged}/>
+    {
+      data.map((mosnter)=>{
+       return <h1 key={mosnter.name}> {mosnter.name}</h1>
+      })
+    }
     </div>
   );
 }
-
+}
 export default App;
+
