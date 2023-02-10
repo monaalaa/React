e20228342d85f4f9868875a3d40d12a2e7adcdf6
monaalaa <106662972+MonaBindary1@users.noreply.github.com>
2023-02-10 17:10:59 +0100

    devided everything to component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
-  constructor()
-  {
+  constructor() {
     super();
-    this.state={
-      monsters:[],
-      searchValue:''
+    this.state = {
+      monsters: [],
+      searchValue: "",
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then((response)=>response.json())
-     .then((users)=>this.setState(()=>{
-      return{ monsters: users}
-     },()=>{
-      console.log(this.state);
-     })
-     ) 
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          }
+        )
+      );
   }
-  OnsearchChanged=(event)=>{
-    const searchValue=event.target.value.toLocaleLowerCase();
-    this.setState(()=>{ return{searchValue}}
-    )
-  };
-  render()
-  {
- const {monsters,searchValue}=this.state;
- const {OnsearchChanged}=this;
-    const data=monsters.filter
-    (monster=>monster.name.toLocaleLowerCase().includes(searchValue));
 
-  return (
-    <div className="App">
-      <input className='searchBox' type='search' placeholder='search mosnters' 
-      onChange={OnsearchChanged}/>
-    {
-      data.map((mosnter)=>{
-       return <h1 key={mosnter.name}> {mosnter.name}</h1>
-      })
-    }
-    </div>
-  );
-}
+  OnsearchChanged = (event) => {
+    const searchValue = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchValue };
+    });
+  };
+
+  render() {
+    const { monsters, searchValue } = this.state;
+    const { OnsearchChanged } = this;
+    const filtredMosters = monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(searchValue)
+    );
+
+    return (
+      <div className="App">
+        
+     <SearchBox 
+     onChangeHandler={OnsearchChanged}
+       placeHolder={'search monster'}
+       className="search-box"/>
+        <CardList monsters={filtredMosters}/>
+      </div>
+    );
+  }
 }
 export default App;
-
